102b3e8a65a0fe62fcde025aa5a8ee81851ee574
gsouza590 <gsouza590@gmail.com>
2024-10-31 16:39:12 -0300

    string-producer started

---
+++ string-producer/src/main/java/com/gabriel/string_producer/StringProducerApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.string_producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringProducerApplication.class, args);
+	}
+
+}
---
+++ string-producer/src/test/java/com/gabriel/string_producer/StringProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.string_producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
