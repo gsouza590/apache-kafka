dc6592ceadf73ba2158c05d45eeb6ad49489a9e9
gsouza590 <gsouza590@gmail.com>
2024-11-25 17:08:54 -0300

    add producer config

--- payment-service/src/main/java/com/gabriel/payment_service/service/impl/PaymentServiceImpl.java
+++ payment-service/src/main/java/com/gabriel/payment_service/service/impl/PaymentServiceImpl.java
@@ -2,14 +2,27 @@ package com.gabriel.payment_service.service.impl;
 
 import com.gabriel.payment_service.model.Payment;
 import com.gabriel.payment_service.service.PaymentService;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
+import org.apache.kafka.common.protocol.types.Field;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import java.io.Serializable;
+
+@RequiredArgsConstructor
 @Log4j2
 @Service
 public class PaymentServiceImpl implements PaymentService {
+    private final KafkaTemplate<String, Serializable>kafkaTemplate;
+
     @Override
+    @SneakyThrows
     public void sendPayment(Payment payment) {
-        log.info("PAYMENT_SERVICE_IMPL ::: Payment Receive", payment);
+        log.info("PAYMENT_SERVICE_IMPL ::: Payment Receive {}", payment);
+        Thread.sleep(1000);
+        log.info("Enviando Pagamento....");
+        kafkaTemplate.send("payment-topic", payment);
     }
 }
